8b5ecb2353073b1e1ef6dfb85957c061fabee30a
michaelprocha <michaelrocha9722@gmail.com>
2025-07-05 13:07:59 -0300

    feat(operations.c): create mathematical operations functions

--- src/operations.c
+++ src/operations.c
@@ -2,18 +2,24 @@
 
 float addition(float n1, float n2)
 {
-
+    float result = n1 + n2;
+    return result;
 }
 
 float subtraction(float n1, float n2)
 {
-
+    float result = n1 - n2;
+    return result;
 }
+
 float multiplication(float n1, float n2)
 {
-
+    float result = n1 * n2;
+    return result;
 }
+
 float division(float n1, float n2)
 {
-    
+    float result = n1 / n2;
+    return result;
 }
