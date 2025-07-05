38be6f3bf9053b0bcec233144df6f61c4d48f865
michaelprocha <michaelrocha9722@gmail.com>
2025-07-05 13:01:55 -0300

    chore: create structure of mathematical functions

--- include/calculator.h
+++ include/calculator.h
@@ -1,10 +1,17 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
+// screens
 void home_screen();
 void addition_screen();
 void subtraction_screen();
 void multiplication_screen();
 void division_screen();
+
+// mathematical operations
+float addition(float n1, float n2);
+float subtraction(float n1, float n2);
+float multiplication(float n1, float n2);
+float division(float n1, float n2);
 
 #endif
