d1999dfc6203ee7593c9c38b211abf8198cb3ba6
michaelprocha <michaelrocha9722@gmail.com>
2025-07-05 11:59:30 -0300

    style: create mathematical operations screens

--- include/calculator.h
+++ include/calculator.h
@@ -2,5 +2,9 @@
 #define CALCULATOR_H
 
 void home_screen();
+void addition_screen();
+void subtraction_screen();
+void multiplication_screen();
+void division_screen();
 
 #endif
--- src/screens.c
+++ src/screens.c
@@ -8,5 +8,68 @@ void home_screen()
     printf("----------------------------- Select the desired mathematical operation ------------------------------\n");
     printf("------------------------------------------------------------------------------------------------------\n");
     printf("------------- (1)Addition -- (2)Subtraction -- (3)Multiplication -- (4)Division ----------------------\n");
-    // scanf 
+}
+
+void addition_screen()
+{
+    //number
+    //number
+    
+    printf("--------------------------------------------- Calculator ---------------------------------------------\n");
+    printf("------------------------------------------------------------------------------------------------------\n");
+    printf("-------------------------------- Enter the first number for operation --------------------------------\n");
+    // scanf
+    printf("-------------------------------- Enter the second number for operation --------------------------------\n");
+    // scanf
+
+    //funcao number number
+
+}
+
+void subtraction_screen()
+{
+    //number
+    //number
+    
+    printf("--------------------------------------------- Calculator ---------------------------------------------\n");
+    printf("------------------------------------------------------------------------------------------------------\n");
+    printf("-------------------------------- Enter the first number for operation --------------------------------\n");
+    // scanf
+    printf("-------------------------------- Enter the second number for operation --------------------------------\n");
+    // scanf
+
+    //funcao number number
+
+}
+
+void multiplication_screen()
+{
+    //number
+    //number
+    
+    printf("--------------------------------------------- Calculator ---------------------------------------------\n");
+    printf("------------------------------------------------------------------------------------------------------\n");
+    printf("-------------------------------- Enter the first number for operation --------------------------------\n");
+    // scanf
+    printf("-------------------------------- Enter the second number for operation --------------------------------\n");
+    // scanf
+
+    //funcao number number
+
+}
+
+void division_screen()
+{
+    //number
+    //number
+    
+    printf("--------------------------------------------- Calculator ---------------------------------------------\n");
+    printf("------------------------------------------------------------------------------------------------------\n");
+    printf("-------------------------------- Enter the first number for operation --------------------------------\n");
+    // scanf
+    printf("-------------------------------- Enter the second number for operation --------------------------------\n");
+    // scanf
+
+    //funcao number number
+
 }
