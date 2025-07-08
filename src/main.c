72506fa70d291793aada5ca8920cc3cc02b31853
michaelprocha <michaelrocha9722@gmail.com>
2025-07-08 10:45:17 -0300

    feat(main): add loop and math operation screens in main.c

--- src/main.c
+++ src/main.c
@@ -4,10 +4,32 @@
 int main(void)
 {
     int option__operation;
-    
-    home_screen();
+    int result;
 
-    scanf("%d", option__operation);
-    
+    do
+    {
+        home_screen();
+
+        scanf("%d", option__operation);
+
+        switch (option__operation)
+        {
+        case 1:
+            addition_screen();
+            break;
+        case 2:
+            subtraction_screen();
+            break;
+        case 3:
+            multiplication_screen();
+            break;
+        case 4:
+            division_screen();
+        }
+
+        system("cls");
+        system("clear");
+    } while (option__operation > 4 || option__operation < 1);
+
     return 0;
 }
